32bb995bae9ebd811d1f838a70f3c806c6245341
helpdeskrad-png <helpdeskrad@gmail.com>
2026-04-10 00:58:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 from gtts import gTTS
 from PIL import Image, ImageDraw
-from moviepy.editor import *
+from moviepy.editor import ImageClip, AudioFileClip
 
 hooks = [
 "90% log ye nahi jaante...",
@@ -19,36 +19,26 @@ facts = [
 "subah uthte hi phone dekhna stress badhata hai"
 ]
 
-colors = [
-(15,15,15),
-(0,0,40),
-(20,0,0),
-(0,20,0),
-(30,0,30)
-]
-
 text = random.choice(hooks) + " " + random.choice(facts)
-bg = random.choice(colors)
 
 # voice
-tts = gTTS(text=text, lang='hi', slow=False)
+tts = gTTS(text=text, lang='hi')
 tts.save("voice.mp3")
 
-# create frame
-img = Image.new("RGB",(1080,1920),bg)
+# image
+img = Image.new("RGB",(1080,1920),(20,20,20))
 draw = ImageDraw.Draw(img)
 
 draw.text((80,200),"Did You Know?", fill=(255,255,0))
 draw.text((80,900), text, fill=(255,255,255))
-draw.text((80,1700),"FactoToons", fill=(255,0,0))
 
 img.save("frame.png")
 
-# create video
+# video
 audio = AudioFileClip("voice.mp3")
 clip = ImageClip("frame.png").set_duration(audio.duration)
 video = clip.set_audio(audio)
 
 video.write_videofile("short.mp4", fps=24)
 
-print("Video created")
+print("Video generated")
