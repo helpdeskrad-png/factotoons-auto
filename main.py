772fc7ee4e9130027c80a222743493c62dd36ed7
helpdeskrad-png <helpdeskrad@gmail.com>
2026-04-10 00:30:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
-import random
-from gtts import gTTS
-from PIL import Image, ImageDraw
-
-facts = [
-"Aapka brain raat ko zyada active hota hai",
-"Human body me 206 bones hoti hai",
-"Overthinking intelligent logo ki habit hoti hai",
-"Sleep kam ho to memory weak hoti hai",
-"90% log phone use karte waqt time bhool jate hai"
+hooks = [
+"90% log ye nahi jaante...",
+"Ye fact aapko shock kar dega...",
+"Agar aap intelligent ho to...",
+"Science ke according...",
+"Ye sach hai aur shocking bhi..."
 ]
 
-text = random.choice(facts)
+facts = [
+"human brain raat ko zyada active hota hai",
+"overthinking intelligent logo ki habit hoti hai",
+"sleep kam ho to memory weak hoti hai",
+"90% log phone use karte waqt time bhool jate hai",
+"subah uthte hi phone dekhna stress badhata hai"
+]
 
-# voice
-tts = gTTS(text=text, lang='hi')
-tts.save("voice.mp3")
-
-# shorts image
-img = Image.new("RGB",(1080,1920),(0,0,0))
-draw = ImageDraw.Draw(img)
-
-draw.text((80,900), text, fill=(255,255,255))
-
-img.save("short.png")
-
-print("Short ready:", text)
+text = random.choice(hooks) + " " + random.choice(facts)
