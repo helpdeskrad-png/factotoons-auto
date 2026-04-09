aa88993b43f6cb546a27116d159c61f80ed59fa6
helpdeskrad-png <helpdeskrad@gmail.com>
2026-04-09 22:44:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import random
 from gtts import gTTS
+from moviepy.editor import *
+from PIL import Image, ImageDraw, ImageFont
 
 facts = [
+"Aapka brain raat ko zyada active hota hai",
 "Human body me 206 bones hoti hai",
-"Aapka brain 75% water se bana hai",
-"Sleep ke bina brain shrink ho sakta hai",
-"Heart ek din me 100000 baar beat karta hai",
+"Overthinking intelligent logo ki habit hoti hai",
+"Sleep kam ho to memory weak hoti hai",
 "90% log phone use karte waqt time bhool jate hai"
 ]
 
 text = random.choice(facts)
 
+# create voice
 tts = gTTS(text=text, lang='hi')
 tts.save("voice.mp3")
 
-print("voice generated")
+# create image
+img = Image.new("RGB",(1080,1920),(0,0,0))
+draw = ImageDraw.Draw(img)
+draw.text((100,900), text, fill=(255,255,255))
+img.save("frame.png")
+
+# create video
+audio = AudioFileClip("voice.mp3")
+clip = ImageClip("frame.png").set_duration(audio.duration)
+video = clip.set_audio(audio)
+
+video.write_videofile("short.mp4", fps=24)
+
+print("video created")
